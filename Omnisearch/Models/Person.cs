be69d8fb0969e7a378d3c5cba53b9114f59e3e65
Ethan Chen <jonathanchenzheng96@gmail.com>
2023-07-26 22:07:21 -0400

    Create boilerplate

---
+++ Omnisearch/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿namespace Omnisearch.Models;
+
+public class Person
+{
+    public int Id { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string EmailAddress { get; set; }
+
+    public Person(int id, string firstName, string lastName, string emailAddress)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        EmailAddress = emailAddress;
+    }
+}
--- Omnisearch/Program.cs
+++ Omnisearch/Program.cs
@@ -1,6 +1,43 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Nest;
+using System;
+using Omnisearch.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddSingleton<IElasticClient>(provider =>
+{
+    var settings = new ConnectionSettings(new Uri("http://localhost:9200"));
+    return new ElasticClient(settings);
+});
+
+// Add controllers
+builder.Services.AddControllers();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Create Elasticsearch Index
+var client = app.Services.GetRequiredService<IElasticClient>();
+var createIndexResponse = client.Indices.Create("people", c => c
+    .Map<Person>(m => m
+        .AutoMap()
+    )
+);
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
 
 app.Run();
